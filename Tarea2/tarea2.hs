370495cb33f491cd79811f6b544242c62e7e1c11
Damián González <dg164553@fi365.ort.edu.uy>
2026-05-03 18:08:26 -0300

    Tarea 2 completa

--- Tarea2/tarea2.hs
+++ Tarea2/tarea2.hs
@@ -86,15 +86,25 @@ suma_i n  = suma_entre_f (\x -> x) 0 n
 
 --1.
 es_divisor :: N -> N -> Bool
-es_divisor n k = undefined
+es_divisor n k
+    | k > n = False
+    | k == 0 = False
+    | n == k = True
+    | otherwise = es_divisor (n-k) k
 
 --2.
 primer_divisor :: N -> N
-primer_divisor n = undefined
+primer_divisor n = primer_divisor_desde n 2
+
+primer_divisor_desde :: N -> N -> N
+primer_divisor_desde n k
+    | es_divisor n k = k
+    | otherwise = primer_divisor_desde n (k + 1)
+-- Tuve que usar una funcion auxiliar para poder arrancar desde 2 el llamado	
 
 --3.
 es_primo :: N -> Bool
-es_primo n = undefined
+es_primo n = primer_divisor n == n
 
 --------------
 --PROBLEMA 3--
@@ -108,17 +118,20 @@ minimo_acotado p m n
 
 --1.
 {--
-Si ningun valor en el intervalo considerado cumple el predicado, entonces...
+Si ningun valor en el intervalo considerado cumple el predicado, retorna un valor fuera del intervalo (n + 1), 
+lo cual sirve como indicador de que no se encontró ningún elemento.
 --}
 
 --2.
 primer_divisor' :: N -> N
-primer_divisor' n = undefined
+primer_divisor' n = minimo_acotado (es_divisor n) 2 n
 
 --3.
 maximo_acotado :: (N -> Bool) -> N -> N -> N
-maximo_acotado p m n = undefined
-
+maximo_acotado p m n
+    | m > n               = n
+    | m <= n && p n       = n
+    | m <= n && not (p n) = maximo_acotado p m (n - 1)
 --4.
 minimo_p :: (N -> Bool) -> N -> N
 minimo_p p n
@@ -126,7 +139,8 @@ minimo_p p n
 	| not (p n) = minimo_p p (n+1)
 
 {--
-Esta función termina si...
+Esta función termina si existe algún k >= n tal que p k sea True. 
+Si no existe ninguno entra en loop infinito, ya que no tiene cota superior que la detenga
 --}
 
 --------------
@@ -135,27 +149,45 @@ Esta función termina si...
 
 --1.
 cantidad_p :: (N -> Bool) -> N -> N -> N
-cantidad_p p m n = undefined
+cantidad_p p m n
+    | m > n     = 0
+    | p m       = 1 + cantidad_p p (m + 1) n
+    | otherwise = cantidad_p p (m + 1) n
 
 --2.
 suma_p :: (N -> Bool) -> N -> N -> N
-suma_p p m n = undefined
+suma_p p m n
+    | m > n     = 0
+    | p m       = m + suma_p p (m + 1) n
+    | otherwise = suma_p p (m + 1) n
 
 --3.
 suma2_p :: (N -> Bool) -> N -> N -> N
-suma2_p p m n = undefined
+suma2_p p m n
+    | m > n     = 0
+    | p m       = m * m + suma2_p p (m + 1) n
+    | otherwise = suma2_p p (m + 1) n
 
 --4.
 sumaf_p :: (N -> Bool) -> (N -> N) -> N -> N -> N
-sumaf_p p f m n = undefined
+sumaf_p p f m n
+    | m > n     = 0
+    | p m       = f m + sumaf_p p f (m + 1) n
+    | otherwise = sumaf_p p f (m + 1) n
 
 --5.
 todos_p :: (N -> Bool) -> N -> N -> Bool
-todos_p p m n = undefined
+todos_p p m n
+    | m > n     = True
+    | p m       = todos_p p (m + 1) n
+    | otherwise = False
 
 --5.
 existe_p :: (N -> Bool) -> N -> N -> Bool
-existe_p p m n = undefined
+existe_p p m n
+    | m > n     = False
+    | p m       = True
+    | otherwise = existe_p p (m + 1) n
 
 -------
 --FIN--
