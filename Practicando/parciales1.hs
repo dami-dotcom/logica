951c637cf5386eb3019f751fe009472c468f1c70
Damián González <damian.gonzalez@outlook.com>
2026-05-12 04:43:03 -0300

    Mas parciales

--- Practicando/parciales1.hs
+++ Practicando/parciales1.hs
@@ -124,3 +124,107 @@ sumaCre :: Int -> Int -> Int
 sumaCre n x
   | n == 0 = 0
   | n > 0  = x + sumaCre (n - 1) (x + 1)
+
+-- 20241014-Par-Mat-LyMD.pdf
+-- 14/10/2024 Matutino
+
+{--
+1. Hacer la tabla de verdad y programar en Haskell la siguiente conectiva ternaria:
+ter :: Bool -> Bool -> Bool -> Bool, que dados x, y, z de tipo Bool, retorna True si al menos 2 de sus
+argumentos son False o cuando z es False.
+--}
+
+ter :: Bool -> Bool -> Bool -> Bool
+ter False False True    = True
+ter _     _     False   = True
+ter _     _     _       = False
+
+
+{--
+1.  Definir la funcion negar :: Bool -> N -> Bool, que dado un booleano x y un natural n, 
+    aplica n veces la funcion not al parametro x.
+Ejemplo:
+negar True 3 = not(not(not(True))) = False
+negar True 0 = True
+--}
+
+negar :: Bool -> N -> Bool
+negar a x
+    | x == 0 = a
+    | otherwise = negar (not a) (x-1)
+
+{--
+2.  Definir la funcion noCumple :: (N -> Bool) -> N -> N -> Bool, que dado un predicado p 
+    y dos naturales m y n, devuelve True si y solo si existe un natural i 
+    perteneciente al intervalo cerrado [m,n] que no cumple con el predicado p.
+
+Ejemplos:
+noCumple (<4) 2 4 = True
+noCumple (>0) 2 4 = False
+--}
+
+noCumple :: (N -> Bool) -> N -> N -> Bool
+noCumple p m n
+    | m > n = error "No tiene sentido el rango"
+    | m == n = not (p m)
+    | otherwise =  (noCumple p (m + 1) n) || not (p m)
+
+-- 20241014-Par-Noc-LyMD (1).pdf
+-- 14/10/2024
+{--
+Problema 1. Dada la siguiente tabla de verdad:
+p q r ϕ(p,q,r)
+T T T T
+T T F T
+T F T F
+T F F T
+F T T T
+F T F F
+F F T T
+F F F T
+
+1. Programar sin hacer uso de funciones auxiliares la funcion phi :: Bool -> Bool -> Bool -> Bool
+representada por la tabla de verdad definida previamente.
+2. Reprogramar la funcion phi :: Bool -> Bool -> Bool -> Bool en dos lıneas, haciendo uso unicamente 
+de la funcion implica (>>) :: Bool -> Bool -> Bool.
+--}
+
+phi :: Bool -> Bool -> Bool -> Bool -- implica not x or y
+phi True True True = True       -- (not true or True) = True // not True or True = True 
+phi True True False = True      -- (not true or True) = True // not True or False = True
+phi True False True = False     -- (not true or False) = False // not False or True = False  
+phi True False False = True     -- (not true or False) = False // not False or False
+phi False True True = True
+phi False True False = False
+phi False False True = True
+phi False False False = True
+
+implica :: Bool -> Bool -> Bool
+implica x y = not x || y
+
+phi' :: Bool -> Bool -> Bool -> Bool
+phi' x y z = implica x (implica y z)
+
+-- LyMD_Agosto2023Noc.pdf
+{--
+1. Definir la funcion fact :: Int -> Int, que dado un natural n, retorna el factorial de n, definido como
+    n! = n*(n-1)*(n-2)*...*1.
+Ejemplo: fact 6 = 6*5*4*3*2*1.
+
+--}
+fact :: Int -> Int
+fact x
+    | x == 0 = 1
+    | otherwise = x * fact (x - 1)
+
+{--
+2. Definir la funcion prodEntre :: Int -> Int -> Int, que dados dos naturales m y n, 
+retorna el producto de
+todos los numeros que se encuentren entre m y n. Puede asumir que m ≤ n.
+Ejemplo: prodEntre 3 6 = 3*4*5*6.
+--}
+
+prodEntre :: Int -> Int -> Int
+prodEntre m n
+    | m > n = 1
+    | otherwise = m * prodEntre (m +1 ) n
